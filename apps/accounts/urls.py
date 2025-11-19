11be6a67627804680a614b23cf9eb446eeb5d653
Varivonchik Artem <varivonchik.core@gmail.com>
2025-11-19 22:18:37 +0300

    feat(urls): setup urls for login

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("register/", views.UserRegistrationView.as_view(), name="register")]
+urlpatterns = [
+    path("register/", views.UserRegistrationView.as_view(), name="register"),
+    path("login/", views.UserLoginView.as_view(), name="login"),
+]
