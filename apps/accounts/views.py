bc240acb66ce1563496fc55db4c32d255c63751f
Varivonchik Artem <varivonchik.core@gmail.com>
2025-11-19 22:05:52 +0300

    feat(accounts): implement UserLoginView

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,12 +1,13 @@
 from typing import cast
 
+from django.contrib.auth import login
 from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from apps.accounts.serializers import UserRegistrationSerializer
+from apps.accounts.serializers import UserLoginSerializer, UserRegistrationSerializer
 
 
 class UserRegistrationView(APIView):
@@ -28,3 +29,29 @@ class UserRegistrationView(APIView):
         }
 
         return Response(response_data, status=status.HTTP_201_CREATED)
+
+
+class UserLoginView(APIView):
+    def post(self, request):
+        serializer = UserLoginSerializer(
+            data=request.data, context={"request": request}
+        )
+        serializer.is_valid(raise_exception=True)
+
+        validated_data = cast(dict, serializer.validated_data)
+
+        user: User = validated_data["user"]
+
+        login(request, user)
+        refresh = RefreshToken.for_user(user)
+
+        response_data = {
+            "user": UserRegistrationSerializer(user).data,
+            "tokens": {
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+                "message": "You have successfully logged in",
+            },
+        }
+
+        return Response(response_data, status=status.HTTP_200_OK)
